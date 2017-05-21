3d022161dcab73ed75f0a4844808114938f27f14
Conroy Whitney <conroy.whitney@gmail.com>
2017-05-20 18:09:45 -0800

    3. Wiring up the schema to the server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
+import bodyParser from 'body-parser';
+
+import { schema } from './src/schema';
 
 const PORT = 4000;
 
 const server = express();
+
+server.use('/graphql', bodyParser.json(), graphqlExpress({
+  schema
+}));
 
 server.get('/', function (req, res) {
   res.send('Hello World!');
